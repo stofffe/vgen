7838334d485237b4872059fa14f16d306e97c936
Christoffer Andersson <chan8@kth.se>
2024-07-12 12:14:25 +0100

    added custom rules example

---
+++ examples/custom/custom.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/stofffe/vgen"
+)
+
+// vgen
+type Vector struct {
+	vector []float32
+}
+
+// Custom rule
+// Components must sum to 1
+func Normalized() vgen.RuleFunc[[]float32] {
+	return func(field_name string, input []float32) vgen.ErrorMap {
+		var sum float32
+		for _, v := range input {
+			sum += v
+		}
+		if sum != 1.0 {
+			return vgen.NewErrorMap(field_name, fmt.Errorf("vector not normalized"))
+		}
+		return nil
+	}
+}
+
+func main() {
+
+	vector := VectorVgen{
+		vector: &[]float32{1, 0, 1},
+	}
+
+	rules := VectorRules{
+		// vector: vgen.NewRules(true,
+		// 	vgen.LenEq[[]float32](3),
+		// 	Normalized(),
+		// ),
+		vector: vgen.RequiredRules(
+			vgen.LenEq[[]float32](3),
+			Normalized(),
+		),
+	}
+
+	err := vector.Validate(rules)
+	fmt.Println(err.Debug())
+}
